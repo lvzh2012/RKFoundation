f39ee0172ff8140d474058b1608d91ade30d547d
zhenhua <zhenhua>
2026-01-06 18:30:55 +0800

    github release zip

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "RKFoundation",
-            url: "https://github.com/your-username/RKFoundation/releases/download/1.0.0/RKFoundation.xcframework.zip",
+            url: "https://github.com/lvzh2012/RKFoundation/releases/download/1.0.0/RKFoundation.xcframework.zip",
             checksum: "48db0bfc2b0afef21335e35ff17c8cad6a379ec4776bbec0127ceb0b9e5bc770"
         ),
     ]
